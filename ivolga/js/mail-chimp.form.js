aa2015f0f652c154bad9f14578ea86a33d9109e1
technokon <iakoupov@gmail.com>
2020-10-04 23:45:54 -0400

    new images and some form styling and functionality

---
+++ ivolga/js/mail-chimp.form.js
@@ -0,0 +1,21 @@
+
+(function($) {
+    window.fnames = new Array();
+    window.ftypes = new Array();
+    fnames[1]='MMERGE1';
+    ftypes[1]='text';
+    fnames[3]='MMERGE3';
+    ftypes[3]='text';
+    fnames[0]='EMAIL';
+    ftypes[0]='email';
+    fnames[5]='MMERGE5';
+    ftypes[5]='url';
+    fnames[6]='MMERGE6';
+    ftypes[6]='text';
+    fnames[4]='PHONE';
+    ftypes[4]='phone';
+    fnames[2]='MMERGE2';
+    ftypes[2]='radio';
+}(jQuery));
+var $mcj = jQuery.noConflict(true);
+
